3f0aba355afb1b6ae10850eacd25053bf36c2fff
Yizhou <yizhouzhao@g.ucla.edu>
2020-08-22 17:13:13 -0700

    rebuild

--- Build/AnimLabeling.framework.js
+++ Build/AnimLabeling.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3379840;
+STATICTOP = STATIC_BASE + 3380496;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3227,7 +3227,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3379840;
+var STATIC_BUMP = 3380496;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
